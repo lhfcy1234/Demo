8b4d4071bb7ce21193c1a8ed6a75faa3be1d3ea6
lh <liuhao_web@163.com>
2018-07-09 23:41:44 +0800

    222

---
+++ src/components/Layout.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="b">
+      <el-row>
+        <el-col :span="12" class="aaa" ref="c"> wwwww</el-col>
+      </el-row>
+      <div ref="d"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Layout",
+        mounted (){
+         let aHeight = document.body.clientHeight;
+          console.log(aHeight)
+          console.log( this.$refs.c)
+          //this.$refs.c.style.height = aHeight+'px';
+          this.$refs.d.style.height = aHeight+'px';
+          //普通的元素上有style 组件上面没这个属性所以
+          //this.$refs.c.style.height = aHeight+'px'; 报错
+          //this.$refs.c.style为undefined
+        }
+    }
+</script>
+
+<style scoped>
+  .b{
+    height: 500px;
+  }
+  .aaa{
+    background:red;
+    border-right:1px solid blue;
+  }
+</style>
